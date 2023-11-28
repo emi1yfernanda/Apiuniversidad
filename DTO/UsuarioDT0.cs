4e2534fd9cd5078f2837316552c5a095a11ac7d2
emi1yfernanda <f.emily@escolar.ifrn.edu.br>
2023-11-28 11:51:02 -0300

    aa

---
+++ DTO/UsuarioDT0.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apiuniversidade.DTO
+{
+    public class UsuarioDT0
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassoword { get; set; }
+        
+    }
+}
